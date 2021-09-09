3cd4bb5eb37ca0ff92281a121e0bfb495e126479
Elacosta <neero5500@gmail.com>
2021-09-09 16:40:46 -0300

    adição class Calculator

---
+++ src/br/com/dio/calculator/Calculator.java
@@ -0,0 +1,51 @@
+package br.com.dio.calculator;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a, b;
+
+        System.out.println("Digite o primeiro valor: ");
+        a = scan.nextInt();
+        System.out.println("Digite o segundo valor: ");
+        b = scan.nextInt();
+
+        int somar = somar(a, b);
+
+        int subtrair = subtrair(a, b);
+
+        int multiplicar = multiplicar(a, b);
+
+        int dividir = dividir(a, b);
+
+        System.out.println("soma: " + somar);
+        System.out.println("Subtração: " + subtrair);
+        System.out.println("Multiplicação: " + multiplicar);
+        System.out.println("Divisão: " + dividir);
+
+    }
+
+    public static int somar(int a, int b) {
+        return (a+b);
+    }
+
+    public static int subtrair(int a, int b) {
+        return (a-b);
+    }
+
+    public static int multiplicar(int a, int b) {
+        return (a*b);
+    }
+
+    public static int dividir(int a, int b) {
+        return (a/b);
+    }
+
+
+
+
+}
