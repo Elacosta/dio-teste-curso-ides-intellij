ce64f7554ff8c0b3ac5faf25660802e9d02f5bcb
Elacosta <neero5500@gmail.com>
2021-09-08 21:53:56 -0300

    class Book adicionado

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -6,13 +6,49 @@ public class PrimeiroPrograma {
     public static void main(String[] args) {
 
         Cat cat = new Cat();
-
         System.out.println(cat);
+
+        Book book = new Book("Contos de tchutchulo", 250);
+
 
         /*
         int a = 5;
         int b = 3;
 
         System.out.println("Hello world!" + (a+b));*/
+    }
+}
+
+class Book {
+    private String name;
+    private Integer numPages;
+
+    public Book(String name, Integer numPages) {
+        this.name = name;
+        this.numPages = numPages;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getNumPages() {
+        return numPages;
+    }
+
+    public void setNumPages(Integer numPages) {
+        this.numPages = numPages;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", numPages='" + numPages + '\'' +
+                '}';
     }
 }
